22655d5feefa7631e475ec67a299e4f89d925189
Rona Hurvits <ronahurvits@Ronas-MacBook-Pro.local>
2024-11-21 15:38:30 +0200

    another fix

--- Collatz.java
+++ Collatz.java
@@ -1,24 +1,22 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-		int myNum = Integer.parseInt(args[0]);
-		String finOut = args[0];
-		int myNumRange = Integer.parseInt(args[0]);
-		for (int i = 1; i <= myNum; i++) {
-			myNum += 1;
-			// while (myNum > 1) {
-			// 	// System.out.println("test" +myNum);
-			// 	if (myNum%2==0) myNum = myNum/2; else myNum= (myNum*3) +1;
-			// 	finOut = finOut + ", " + myNum;
-			// }
-			while (myNum > 1) {
-				// System.out.println("test" +myNum);
-				if (myNum%2==0) myNum = myNum/2; else myNum= (myNum*3) +1;
-				finOut = finOut + ", " + myNum;
+		if (args[1].indexOf("v")!= -1){
+			int myNum = Integer.parseInt(args[0]);
+			int myNumRange = Integer.parseInt(args[0]);
+			for (int i = 1; i <= myNumRange ; i++) {
+			String finOut = ""+i;	
+			int finOutCount = 1;
+			myNum = i;
+				do {
+					finOutCount += 1;
+					// System.out.println("test count " +finOutCount);
+					// System.out.println("test" +myNum+ "round "+ i);
+					if (myNum%2==0) myNum = myNum/2; else myNum= (myNum*3) +1;
+					finOut = finOut + " " + myNum;}
+				while (myNum > 1);
+				System.out.println(finOut + " (" + finOutCount + ")");
 			}
-			
-			System.out.println(finOut);
 		}
 		System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1");
 	}
