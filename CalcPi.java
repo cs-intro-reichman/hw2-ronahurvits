b4d0f94ec0d79c4ff048a296f1056cafccd25a9b
Rona Hurvits <ronahurvits@Ronas-MacBook-Pro.local>
2024-11-21 08:46:10 +0200

    very initial draft

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,16 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int iterations = Integer.parseInt(args[0]);
+		double one = (double) 1;
+		int initDenominator = 3;
+		double calcPi = 1.0;
+		for (int i = 0; i < iterations; i++) {
+			if (i%2 == 0) {calcPi -= (one/initDenominator);}
+			if (i%2 != 0) {calcPi += (one/initDenominator);}
+			initDenominator +=2;
+		}
+		System.out.println("pi according to Java: "+Math.PI);
+		System.out.println("pi, approximated: "+ calcPi*4);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,24 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int myNum = Integer.parseInt(args[0]);
+		String finOut = args[0];
+		int myNumRange = Integer.parseInt(args[0]);
+		for (int i = 1; i <= myNum; i++) {
+			myNum += 1;
+			// while (myNum > 1) {
+			// 	// System.out.println("test" +myNum);
+			// 	if (myNum%2==0) myNum = myNum/2; else myNum= (myNum*3) +1;
+			// 	finOut = finOut + ", " + myNum;
+			// }
+			while (myNum > 1) {
+				// System.out.println("test" +myNum);
+				if (myNum%2==0) myNum = myNum/2; else myNum= (myNum*3) +1;
+				finOut = finOut + ", " + myNum;
+			}
+			
+			System.out.println(finOut);
+		}
+		System.out.println("Every one of the first " + args[0] + " hailstone sequences reached 1");
 	}
 }
